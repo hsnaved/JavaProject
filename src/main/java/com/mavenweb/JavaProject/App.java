19f397518da83b57a08d42b041108d2fa7ea79e2
saidevansh <saidevansh023@gmail.com>
2024-01-29 17:35:49 +0530

    firstcommit

---
+++ src/main/java/com/mavenweb/JavaProject/App.java
@@ -0,0 +1,13 @@
+package com.mavenweb.JavaProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
